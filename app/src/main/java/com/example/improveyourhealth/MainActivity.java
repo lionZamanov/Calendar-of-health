5b302ebe9a6338037bc434c236b0574771fea9ea
lionZamanov <lionzamanov@gmail.com>
2017-11-18 22:13:58 +0300

    add Project

---
+++ app/src/main/java/com/example/improveyourhealth/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.improveyourhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+
+        }
+    }
